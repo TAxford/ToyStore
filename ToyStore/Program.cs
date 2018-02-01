b5a2e0b972465a113234c42d7f3b1a8d399ba07c
TAxford <t.axford2@yahoo.com>
2018-02-01 11:38:37 -0500

    Initial commit

---
+++ ToyStore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] toyArryay = { "action figure", "doll", "legos" };
+
+            List<string> toyList = new List<string>() { }
+        }
+    }
+}
